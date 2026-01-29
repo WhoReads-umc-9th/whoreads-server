60c16d4296a5fb6035d1902be1efc1e9edae0ef2
yujin <yujin09219@naver.com>
2026-01-29 15:42:05 +0900

    feat: 사용자 FCM 토큰 관련 API 설계

--- src/main/java/whoreads/backend/domain/member/controller/MemberController.java
+++ src/main/java/whoreads/backend/domain/member/controller/MemberController.java
@@ -1,14 +1,44 @@
 package whoreads.backend.domain.member.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import whoreads.backend.domain.member.controller.docs.MemberControllerDocs;
+import whoreads.backend.domain.member.dto.MemberRequest;
 import whoreads.backend.domain.member.service.MemberService;
+import whoreads.backend.domain.notification.service.NotificationTokenService;
+import whoreads.backend.global.response.ApiResponse;
+
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
-public class MemberController {
+public class MemberController implements MemberControllerDocs {
 
     private final MemberService memberService;
+    private final NotificationTokenService notificationTokenService;
+
+    @PostMapping ("me/fcm-tokens")
+    @Override
+    public ApiResponse<Void> updateFcmToken(
+            // AUTH FILTER가 작동되면 userId를 제거하고 주석을 삭제해주세요!
+//            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @RequestParam Long userId,
+            @RequestBody @Valid MemberRequest.FcmTokenRequest fcmRequest) {
+
+        // notificationTokenService.updateToken(userPrincipal.getId(),fcmRequest.fcmToken());
+        notificationTokenService.updateToken(userId,fcmRequest.fcmToken());
+        return ApiResponse.success("토큰이 성공적으로 등록되었습니다.");
+    }
+    @DeleteMapping("me/fcm-tokens")
+    @Override
+    public ApiResponse<Void> deleteFcmToken(
+            // AUTH FILTER가 작동되면 userId를 제거하고 주석을 삭제해주세요!
+//            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @RequestParam Long userId) {
+
+        // notificationTokenService.deleteToken(userPrincipal.getId());
+        notificationTokenService.deleteToken(userId);
+        return ApiResponse.success("토큰이 성공적으로 삭제되었습니다.");
+    }
 }
---
+++ src/main/java/whoreads/backend/domain/member/controller/docs/MemberControllerDocs.java
@@ -0,0 +1,37 @@
+package whoreads.backend.domain.member.controller.docs;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestParam;
+import whoreads.backend.domain.member.dto.MemberRequest;
+import whoreads.backend.global.response.ApiResponse;
+
+@Tag(name = "Member (사용자)")
+public interface MemberControllerDocs {
+
+    @Operation(summary = "토큰 업데이트",
+            description = "푸시 알림 발송에 필요한 토큰을 등록 또는 업데이트 합니다.")
+    @ApiResponses(
+            {@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+//            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "실패")
+            })
+    ApiResponse<Void> updateFcmToken(
+            // AUTH FILTER가 작동되면 userId를 제거하고 주석을 삭제해주세요!
+            // @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @RequestParam Long userId,
+            @RequestBody MemberRequest.FcmTokenRequest request
+    );
+    @Operation(summary = "토큰 삭제",
+            description = "사용자가 앱을 삭제하거나 로그아웃 할 시 토큰을 삭제합니다.")
+    @ApiResponses(
+            {@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공"),
+//            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "실패")
+            })
+    ApiResponse<Void> deleteFcmToken(
+            // AUTH FILTER가 작동되면 userId를 제거하고 주석을 삭제해주세요!
+            // @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @RequestParam Long userId
+    );
+}
